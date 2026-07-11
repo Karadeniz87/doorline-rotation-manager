1d8cf6434cf6c91e2503e373f75d8b154de8f398
Karadeniz87 <playstation.4@live.de>
2026-07-11 13:24:07 +0200

    Rename employee assignments in station class

--- station.py
+++ station.py
@@ -7,9 +7,9 @@ class Station(BaseModel):
 
     active: bool = True
 
-    double_takt_allowed: bool = False
+    double_takt: bool = False
 
-    assigned_employee_1: Optional[str] = None
-    assigned_employee_2: Optional[str] = None
+    assigned_employee_left: Optional[str] = None
+    assigned_employee_right: Optional[str] = None
 
     support_required: bool = False
