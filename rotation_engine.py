14f746bab4bfb5dc664572d44eb53ac0333d384a
Karadeniz87 <playstation.4@live.de>
2026-07-11 14:10:58 +0200

    Update rotation_engine.py

--- rotation_engine.py
+++ rotation_engine.py
@@ -1,14 +1,32 @@
-DOUBLE_TAKT_STATIONS = {
-    "40L", "40R",
-    "50L", "50R",
-    "60L", "60R",
-    "70L", "70R"
-}
-
-
 def run_rotation(employees, stations):
+
+    available_employees = [
+        e for e in employees
+        if e.get("status") == "Verfügbar"
+    ]
+
+    employee_index = 0
+
+    for station in stations:
+
+        if not station["active"]:
+            continue
+
+        station["employee_1"] = None
+        station["employee_2"] = None
+
+        if employee_index < len(available_employees):
+            employee = available_employees[employee_index]
+
+            station["employee_1"] = (
+                employee["firstname"] +
+                " " +
+                employee["lastname"]
+            )
+
+            employee_index += 1
+
     return {
-        "message": "Rotation Engine bereit",
-        "employees": len(employees),
-        "stations": len(stations)
+        "assigned_employees": employee_index,
+        "stations": stations
     }
