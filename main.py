8cf909e738237001088912b951a2747de28f0c84
Karadeniz87 <playstation.4@live.de>
2026-07-11 12:05:51 +0200

    Add delete_employee endpoint to remove an employee

--- main.py
+++ main.py
@@ -25,3 +25,15 @@ def get_employees():
 def add_employee(employee: Employee):
     employees.append(employee.model_dump())
     return employee
+@app.delete("/employees/{employee_id}")
+def delete_employee(employee_id: int):
+    if employee_id < len(employees):
+        deleted = employees.pop(employee_id)
+        return {
+            "message": "Mitarbeiter gelöscht",
+            "employee": deleted
+        }
+
+    return {
+        "message": "Mitarbeiter nicht gefunden"
+    }
