bb23282a3a1e765eb76829c96dbd022129e6cc5f
Karadeniz87 <playstation.4@live.de>
2026-07-11 16:27:07 +0200

    Update database.py

--- database.py
+++ database.py
@@ -6,7 +6,9 @@ DATABASE_URL = "sqlite:///./database.db"
 
 engine = create_engine(
     DATABASE_URL,
-    connect_args={"check_same_thread": False}
+    connect_args={
+        "check_same_thread": False
+    }
 )
 
 SessionLocal = sessionmaker(
