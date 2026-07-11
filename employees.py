4488b281e103ba2fcdaa997f9a194d98811f7750
Karadeniz87 <playstation.4@live.de>
2026-07-11 12:09:03 +0200

    Update employees.py

--- employees.py
+++ employees.py
@@ -1,11 +1,30 @@
-employees = []
+from pydantic import BaseModel
+from typing import Optional
 
-def add_employee(firstname, lastname):
-    employees.append({
-        "firstname": firstname,
-        "lastname": lastname,
-        "status": "Verfügbar"
-    })
+class Employee(BaseModel):
+    firstname: str
+    lastname: str
+    status: str = "Verfügbar"
 
-def get_employees():
-    return employees
+    station: Optional[str] = None
+
+    skill_30L: bool = False
+    skill_30R: bool = False
+    skill_40L: bool = False
+    skill_40R: bool = False
+    skill_50L: bool = False
+    skill_50R: bool = False
+    skill_60L: bool = False
+    skill_60R: bool = False
+    skill_70L: bool = False
+    skill_70R: bool = False
+    skill_80L: bool = False
+    skill_80R: bool = False
+    skill_90L: bool = False
+    skill_90R: bool = False
+    skill_100L: bool = False
+    skill_100R: bool = False
+    skill_110L: bool = False
+    skill_110R: bool = False
+
+    fairness_points: int = 0
