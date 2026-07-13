c621c8e94fa50adfc950cbf4200e25114f8b4c18
Karadeniz87 <playstation.4@live.de>
2026-07-13 07:59:43 +0200

    Update database.py

--- database.py
+++ database.py
@@ -1,14 +1,13 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import declarative_base
 
-DATABASE_URL = "sqlite:///./database.db"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(
     DATABASE_URL,
-    connect_args={
-        "check_same_thread": False
-    }
+    pool_pre_ping=True
 )
 
 SessionLocal = sessionmaker(
